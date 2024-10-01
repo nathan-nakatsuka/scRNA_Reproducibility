9f93548102dfd5c9816db5866b5095d5440191eb
nathan-nakatsuka <08nanaka@gmail.com>
2024-10-01 16:54:45 -0400

    Update PermutingCaseControlStatus.R

--- PermutingCaseControlStatus.R
+++ PermutingCaseControlStatus.R
@@ -1,14 +1,16 @@
 # This code permutes cases and controls. It requires the following: DatasetName (a string with the name of the Dataset in PresenceofDataTable)
-# PresenceofDataTable (a table indicating whether the dataset has cells for each cell type), CellTypeName (a string indicating what is the cell type label, such as "predicted.celltype.l1")
+# PresenceofDataTable (a table indicating whether the dataset has cells for each cell type), CellTypeName (a string indicating what is the cell type label, such as "predicted.celltype.l1"),
+# avg_exp_Dataset is a Seurat object that the user will use for differential expression. This is usually a pseudobulked object (or not pseudobulked if using a mixed model for differential expression).
 # BroadClusterTypes (a vector of strings naming all cell types), CaseName (a string naming what cases are called in the dataset, such as "COVID-19"), and ControlName (a string naming what controls are called in the dataset, such as "healthy").
-# Note: for this code you must call the individual ID as "patient" and the disease name as "disease_status_standard".
+# Note: for this code you must call the individual ID as "patient" and the disease name as "disease_status_standard" in the avg_exp_Dataset Seurat Object.
+# This will output a new Seurat object with the column name disease_status_standard_Permuted
 PermuteCaseControl <- function(DatasetName,avg_exp_Dataset,PresenceofDataTable,CellTypeName,BroadClusterTypes,CaseName,ControlName){
 	individuals = unique(avg_exp_Dataset$patient)
         individuals = na.omit(individuals)
         Individualtodisease_status_standardTable = data.frame(1:length(individuals))
         Individualtodisease_status_standardTable[,1]=individuals
         Individualtodisease_status_standardTable[,2:3]=0
-        ### Find their normal disease_status_standard and Sex
+        ### Copy their normal disease_status_standard
         for(r in 1:nrow(Individualtodisease_status_standardTable)){
           Individualtodisease_status_standardTable[r,2]=avg_exp_Dataset@meta.data[avg_exp_Dataset@meta.data$patient==Individualtodisease_status_standardTable[r,1],]$disease_status_standard[1]
         }
